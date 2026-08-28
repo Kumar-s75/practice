7a838a05b7e9958eec7b865e851ea0ec62ddd601
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-08-28 06:57:49 +0530

    added a project

--- async-rate-limiter/index.js
+++ async-rate-limiter/index.js
@@ -22,11 +22,52 @@ function fakeRequest(url) {
     });
 }
 
-// Testing one API
-fakeRequest("/api/user/1")
-    .then((data) => {
-        console.log("Success:", data);
-    })
-    .catch((error) => {
-        console.error("Error:", error.message);
+// // Testing one API
+// fakeRequest("/api/user/1")
+//     .then((data) => {
+//         console.log("Success:", data);
+//     })
+//     .catch((error) => {
+//         console.error("Error:", error.message);
+//     });
+
+function sleep(ms){
+    return new Promise((resolve)=>{
+    setTimeout(resolve,ms);
     });
+}
+
+async function fetchWithRetry(url,retries){
+    let attempts = 0;
+    while(attempts<=retries){
+        attempts++;
+        try{
+            const data=await fakeRequest(url);
+            return{
+                url:url,
+                status:"success",
+                data:data,
+                attempts:attempts
+
+            };
+        } catch(error){
+            if(attempts>retries){
+                return{
+                    url:url,
+                    status:"failed",
+                    data:error.message,
+                    attempts:attempts
+                };
+                
+            }   
+            const backoff=500*(2**(attempts-1));
+
+            console.log(
+                `${url} failed.  Retrying in ${backoff}ms...`
+            );
+            await sleep(backoff);
+            
+           }
+
+        }
+    }
